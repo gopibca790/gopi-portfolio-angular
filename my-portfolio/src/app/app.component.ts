cf2cfb2274847f341ba4744c1a707165a62e8fde
gopibca790 <gopibca790@mail.com>
2024-01-28 21:30:38 -0800

    layout

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent ],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent, MaincontentComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ my-portfolio/src/app/maincontent/maincontent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maincontent',
+  standalone: true,
+  imports: [],
+  templateUrl: './maincontent.component.html',
+  styleUrl: './maincontent.component.scss'
+})
+export class MaincontentComponent {
+
+}
